ab0dc95c48084694af6aba10cec691a05d3c591a
wangzhengkun <zhengkun@fit2cloud.com>
2022-08-16 16:47:42 +0800

    feat: 修改session配置文件

--- backend/configs/session.go
+++ backend/configs/session.go
@@ -1,8 +1,6 @@
 package configs
 
 type Session struct {
-	SessionKey     string `mapstructure:"session_key"`
-	SessionUserKey string `mapstructure:"session_user_key"`
-	SessionName    string `mapstructure:"session_name"`
-	ExpiresTime    int    `mapstructure:"expires_time"`
+	SessionName string `mapstructure:"session_name"`
+	ExpiresTime int    `mapstructure:"expires_time"`
 }
