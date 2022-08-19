bda8a331826e5ac22aab0e67493113cbce156dcf
zhengkunwang223 <31820853+zhengkunwang223@users.noreply.github.com>
2022-08-19 16:02:58 +0800

    feat: 文件管理页面

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,10 +8,7 @@ import viteCompression from 'vite-plugin-compression';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 import eslintPlugin from 'vite-plugin-eslint';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import importToCDN from 'vite-plugin-cdn-import';
-// import AutoImport from "unplugin-auto-import/vite";
-// import Components from "unplugin-vue-components/vite";
-// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 import DefineOptions from 'unplugin-vue-define-options/vite';
 
 // @see: https://vitejs.dev/config/
@@ -59,37 +56,14 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
             }),
             DefineOptions(),
             // * EsLint 报错信息显示在浏览器界面上
-            eslintPlugin(),
+            eslintPlugin({
+                exclude: ['**/*.js'],
+            }),
             // * vite 可以使用 jsx/tsx 语法
             vueJsx(),
             // * name 可以写在 script 标签上
             VueSetupExtend(),
-            // * demand import element(如果使用了cdn引入,没必要使用element自动导入了)
-            // AutoImport({
-            // 	resolvers: [ElementPlusResolver()]
-            // }),
-            // Components({
-            // 	resolvers: [ElementPlusResolver()]
-            // }),
-            // * cdn 引入（vue、element-plus）
-            importToCDN({
-                modules: [
-                    // vue按需引入会导致依赖vue的插件出现问题(列如:pinia/vuex)
-                    // {
-                    // 	name: "vue",
-                    // 	var: "Vue",
-                    // 	path: "https://unpkg.com/vue@next"
-                    // },
-                    // 使用cdn引入element-plus时,开发环境还是需要在main.js中引入element-plus,可以不用引入css
-                    // {
-                    // 	name: "element-plus",
-                    // 	var: "ElementPlus",
-                    // 	path: "https://unpkg.com/element-plus",
-                    // 	css: "https://unpkg.com/element-plus/dist/index.css"
-                    // }
-                ],
-            }),
-            // * 是否生成包预览
+
             viteEnv.VITE_REPORT && visualizer(),
             // * gzip compress
             viteEnv.VITE_BUILD_GZIP &&
@@ -104,18 +78,9 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
         esbuild: {
             pure: viteEnv.VITE_DROP_CONSOLE ? ['console.log', 'debugger'] : [],
         },
-        // build configure
         build: {
             outDir: 'dist',
-            // esbuild 打包更快，但是不能去除 console.log
             minify: 'esbuild',
-            // minify: "terser",
-            // terserOptions: {
-            // 	compress: {
-            // 		drop_console: viteEnv.VITE_DROP_CONSOLE,
-            // 		drop_debugger: true
-            // 	}
-            // },
             rollupOptions: {
                 output: {
                     // Static resource classification and packaging
