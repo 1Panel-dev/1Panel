653fbd46182ba8d82ec3f9e116f877272fa0a3c7
zhengkunwang223 <31820853+zhengkunwang223@users.noreply.github.com>
2022-08-24 15:33:38 +0800

    feat: 实现文件路径面包屑导航

---
+++ frontend/src/components/bread-crumbs/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bread-crumbs"><slot></slot></div>
+</template>
+<style lang="scss">
+.bread-crumbs {
+    display: flex;
+    &-item {
+        a {
+            text-decoration: none;
+            transition: all 0.4s;
+        }
+    }
+    i {
+        font-size: 12px;
+        margin-left: 5px;
+        margin-right: 5px;
+        line-height: 22px;
+    }
+}
+</style>
