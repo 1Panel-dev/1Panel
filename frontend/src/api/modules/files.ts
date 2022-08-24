64dc7e815f4ba8a6d34a37807f7b22fc6b0a0a9a
zhengkunwang223 <31820853+zhengkunwang223@users.noreply.github.com>
2022-08-24 15:33:38 +0800

    feat: 实现文件路径面包屑导航

--- frontend/src/api/modules/files.ts
+++ frontend/src/api/modules/files.ts
@@ -1,7 +1,6 @@
 import { File } from '@/api/interface/file';
 import http from '@/api';
-import { ResultData } from '@/api/interface';
 
 export const GetFilesList = (params: File.ReqFile) => {
-    return http.post<ResultData<File.File>>('files/search', params);
+    return http.post<File.File>('files/search', params);
 };
