4ced069428bcbd9f174f8dc48d59449404b2bcee
ssongliu <songlius11@163.com>
2022-09-08 11:39:14 +0800

    feat: 监控数据采集落库

---
+++ backend/cron/corn.go
@@ -0,0 +1,21 @@
+package cron
+
+import (
+	"time"
+
+	"github.com/1Panel-dev/1Panel/cron/job"
+	"github.com/1Panel-dev/1Panel/global"
+	"github.com/robfig/cron/v3"
+)
+
+func Run() {
+	nyc, _ := time.LoadLocation("Asia/Shanghai")
+	Cron := cron.New(cron.WithLocation(nyc))
+	_, err := Cron.AddJob("@every 1m", job.NewMonitorJob())
+	if err != nil {
+		global.LOG.Errorf("can not add corn job: %s", err.Error())
+	}
+	Cron.Start()
+
+	global.Corn = Cron
+}
