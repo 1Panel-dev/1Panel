5f9f728d13bfce93dc97f2923b6f8964d461fb36
zhengkunwang223 <zhengkun@fit2cloud.com>
2022-11-18 17:00:15 +0800

    feat: 增加halo phpmyadmin redis-commander 应用

--- backend/app/service/app_utils.go
+++ backend/app/service/app_utils.go
@@ -46,18 +46,18 @@ func execDockerCommand(database model.AppDatabase, dbInstall model.AppInstall, o
 		Auth:          auth,
 		DbParam:       dbConfig,
 	}
-	_, err := cmd.Exec(getSqlStr(database.Key, op, execConfig))
+	out, err := cmd.Exec(getSqlStr(database.Key, op, execConfig))
 	if err != nil {
-		return err
+		return errors.New(out)
 	}
 	return nil
 }
 
-func getSqlStr(key string, operate DatabaseOp, exec dto.ContainerExec) string {
+func getSqlStr(version string, operate DatabaseOp, exec dto.ContainerExec) string {
 	var str string
 	param := exec.DbParam
-	switch key {
-	case "mysql5.7":
+
+	if strings.Contains(version, "5.7") {
 		if operate == Add {
 			str = fmt.Sprintf("docker exec -i  %s  mysql -uroot -p%s  -e \"CREATE USER '%s'@'%%' IDENTIFIED BY '%s';\" -e \"create database %s;\" -e \"GRANT ALL ON %s.* TO '%s'@'%%' IDENTIFIED BY '%s';\" -e \"FLUSH PRIVILEGES;\"",
 				exec.ContainerName, exec.Auth.RootPassword, param.DbUser, param.Password, param.DbName, param.DbName, param.DbUser, param.Password)
@@ -66,7 +66,9 @@ func getSqlStr(key string, operate DatabaseOp, exec dto.ContainerExec) string {
 			str = fmt.Sprintf("docker exec -i  %s  mysql -uroot -p%s   -e \"drop database %s;\"  -e \"drop user %s;\" ",
 				exec.ContainerName, exec.Auth.RootPassword, param.DbName, param.DbUser)
 		}
-	case "mysql8.0":
+	}
+
+	if strings.Contains(version, "8.0") {
 		if operate == Add {
 			str = fmt.Sprintf("docker exec -i  %s  mysql -uroot -p%s  -e \"CREATE USER '%s'@'%%' IDENTIFIED BY '%s';\" -e \"create database %s;\" -e \"GRANT ALL ON %s.* TO '%s'@'%%';\" -e \"FLUSH PRIVILEGES;\"",
 				exec.ContainerName, exec.Auth.RootPassword, param.DbUser, param.Password, param.DbName, param.DbName, param.DbUser)
@@ -76,6 +78,7 @@ func getSqlStr(key string, operate DatabaseOp, exec dto.ContainerExec) string {
 				exec.ContainerName, exec.Auth.RootPassword, param.DbName, param.DbUser)
 		}
 	}
+
 	return str
 }
 
