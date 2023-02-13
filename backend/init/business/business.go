e27c5f26da08a097c434337ebb243eda40b83564
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-02-13 11:38:09 +0400

    feat: 取消启动时自动更新应用

--- backend/init/business/business.go
+++ backend/init/business/business.go
@@ -3,9 +3,17 @@ package business
 import (
 	"github.com/1Panel-dev/1Panel/backend/app/service"
 	"github.com/1Panel-dev/1Panel/backend/global"
+	"github.com/1Panel-dev/1Panel/backend/utils/common"
 )
 
 func Init() {
+	setting, err := service.NewISettingService().GetSettingInfo()
+	if err != nil {
+		global.LOG.Errorf("sync app error: %s", err.Error())
+	}
+	if common.CompareVersion(setting.AppStoreVersion, "0.0") {
+		return
+	}
 	appService := service.AppService{}
 	if err := appService.SyncAppList(); err != nil {
 		global.LOG.Errorf("sync app error: %s", err.Error())
