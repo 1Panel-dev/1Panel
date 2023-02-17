83b163e1f6aa50f999ab489b2b694174420b0e17
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-02-17 15:13:55 +0800

    feat: 下载离线包改为从OSS下载

--- backend/configs/system.go
+++ backend/configs/system.go
@@ -1,17 +1,16 @@
 package configs
 
 type System struct {
-	Port         string `mapstructure:"port"`
-	DbFile       string `mapstructure:"db_file"`
-	DbPath       string `mapstructure:"db_path"`
-	LogPath      string `mapstructure:"log_path"`
-	DataDir      string `mapstructure:"data_dir"`
-	TmpDir       string `mapstructure:"tmp_dir"`
-	Cache        string `mapstructure:"cache"`
-	Backup       string `mapstructure:"backup"`
-	AppRepoOwner string `mapstructure:"app_repo_owner"`
-	AppRepoName  string `mapstructure:"app_repo_name"`
-	EncryptKey   string `mapstructure:"encrypt_key"`
-	BaseDir      string `mapstructure:"base_dir"`
-	Mode         string `mapstructure:"mode"`
+	Port       string `mapstructure:"port"`
+	DbFile     string `mapstructure:"db_file"`
+	DbPath     string `mapstructure:"db_path"`
+	LogPath    string `mapstructure:"log_path"`
+	DataDir    string `mapstructure:"data_dir"`
+	TmpDir     string `mapstructure:"tmp_dir"`
+	Cache      string `mapstructure:"cache"`
+	Backup     string `mapstructure:"backup"`
+	EncryptKey string `mapstructure:"encrypt_key"`
+	BaseDir    string `mapstructure:"base_dir"`
+	Mode       string `mapstructure:"mode"`
+	RepoUrl    string `mapstructure:"repo_url"`
 }
