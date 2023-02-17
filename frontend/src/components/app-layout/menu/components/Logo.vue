06ddf2cca7e4794f6751303ac443ce14d8f06caa
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-02-17 11:27:19 +0800

    feat: 修改 logo

--- frontend/src/components/app-layout/menu/components/Logo.vue
+++ frontend/src/components/app-layout/menu/components/Logo.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="logo flx-center">
-        <img v-show="!isCollapse" src="@/assets/images/1panel-menu.png" />
-        <img v-show="isCollapse" src="@/assets/images/1panel-small.png" />
+        <img v-show="!isCollapse" :src="getLogoUrl(isCollapse)" alt="" />
+        <img v-show="isCollapse" :src="getLogoUrl(isCollapse)" alt="" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { GlobalStore } from '@/store';
+
 defineProps<{ isCollapse: boolean }>();
+const globalStore = GlobalStore();
+
+const getLogoUrl = (isCollapse: boolean) => {
+    const theme = globalStore.$state.themeConfig.theme || 'light';
+    if (isCollapse) {
+        return new URL(`../../../../assets/images/1panel-logo-${theme}.png`, import.meta.url).href;
+    } else {
+        return new URL(`../../../../assets/images/1panel-menu-${theme}.png`, import.meta.url).href;
+    }
+};
 </script>
 
 <style scoped lang="scss">
