554a238fd3111a81e6adbb8934dbc115074339e4
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-02-21 13:48:52 +0800

    fix: 解决删除应用出现两个 Loading 图标的 BUG

--- frontend/src/views/app-store/installed/delete/index.vue
+++ frontend/src/views/app-store/installed/delete/index.vue
@@ -1,42 +1,40 @@
 <template>
-    <div v-loading="loading">
-        <el-dialog
-            v-model="open"
-            :title="$t('commons.button.delete') + ' - ' + appInstallName"
-            width="30%"
-            :close-on-click-modal="false"
-            :before-close="handleClose"
-        >
-            <el-form ref="deleteForm" label-position="left" v-loading="loading">
-                <el-form-item>
-                    <el-checkbox v-model="deleteReq.forceDelete" :label="$t('app.forceDelete')" />
-                    <span class="input-help">
-                        {{ $t('app.forceDeleteHelper') }}
-                    </span>
-                </el-form-item>
-                <el-form-item>
-                    <el-checkbox v-model="deleteReq.deleteBackup" :label="$t('app.deleteBackup')" />
-                    <span class="input-help">
-                        {{ $t('app.deleteBackupHelper') }}
-                    </span>
-                </el-form-item>
-                <el-form-item>
-                    <span v-html="deleteHelper"></span>
-                    <el-input v-model="deleteInfo" :placeholder="appInstallName" />
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="handleClose" :disabled="loading">
-                        {{ $t('commons.button.cancel') }}
-                    </el-button>
-                    <el-button type="primary" @click="submit" :disabled="loading || deleteInfo !== appInstallName">
-                        {{ $t('commons.button.confirm') }}
-                    </el-button>
+    <el-dialog
+        v-model="open"
+        :title="$t('commons.button.delete') + ' - ' + appInstallName"
+        width="30%"
+        :close-on-click-modal="false"
+        :before-close="handleClose"
+    >
+        <el-form ref="deleteForm" label-position="left" v-loading="loading">
+            <el-form-item>
+                <el-checkbox v-model="deleteReq.forceDelete" :label="$t('app.forceDelete')" />
+                <span class="input-help">
+                    {{ $t('app.forceDeleteHelper') }}
                 </span>
-            </template>
-        </el-dialog>
-    </div>
+            </el-form-item>
+            <el-form-item>
+                <el-checkbox v-model="deleteReq.deleteBackup" :label="$t('app.deleteBackup')" />
+                <span class="input-help">
+                    {{ $t('app.deleteBackupHelper') }}
+                </span>
+            </el-form-item>
+            <el-form-item>
+                <span v-html="deleteHelper"></span>
+                <el-input v-model="deleteInfo" :placeholder="appInstallName" />
+            </el-form-item>
+        </el-form>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="handleClose" :disabled="loading">
+                    {{ $t('commons.button.cancel') }}
+                </el-button>
+                <el-button type="primary" @click="submit" :disabled="loading || deleteInfo !== appInstallName">
+                    {{ $t('commons.button.confirm') }}
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 <script lang="ts" setup>
 import { FormInstance } from 'element-plus';
