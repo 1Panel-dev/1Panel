e36cbb0eb7481511a625d671ce19278e4ba122e5
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-03-13 14:27:49 +0800

    feat: 前端增加 msg-info 组件

--- frontend/src/views/host/file-management/process/index.vue
+++ frontend/src/views/host/file-management/process/index.vue
@@ -7,9 +7,8 @@
         :title="$t('file.downloadProcess')"
     >
         <div v-for="(value, index) in res" :key="index">
-            <span>
-                {{ value['percent'] === 100 ? $t('file.downlodSuccess') : $t('file.downloading') }} {{ value['name'] }}
-            </span>
+            <span>{{ value['percent'] === 100 ? $t('file.downlodSuccess') : $t('file.downloading') }}</span>
+            <MsgInfo :info="value['name']" width="250" />
             <el-progress v-if="value['total'] == 0" :percentage="100" :indeterminate="true" :duration="1" />
             <el-progress v-else :text-inside="true" :stroke-width="15" :percentage="value['percent']"></el-progress>
             <span>
@@ -24,6 +23,7 @@
 import { FileKeys } from '@/api/modules/files';
 import { computeSize } from '@/utils/util';
 import { onBeforeUnmount, ref, toRefs } from 'vue';
+import MsgInfo from '@/components/msg-info/index.vue';
 
 const props = defineProps({
     open: {
@@ -63,8 +63,8 @@ const onClose = () => {};
 const initProcess = () => {
     let href = window.location.href;
     let protocol = href.split('//')[0] === 'http:' ? 'ws' : 'wss';
-    let ipLocal = href.split('//')[1].split('/')[0];
-    processSocket = new WebSocket(`${protocol}://${ipLocal}/api/v1/files/ws`);
+    // let ipLocal = href.split('//')[1].split('/')[0];
+    processSocket = new WebSocket(`${protocol}://localhost:9999/api/v1/files/ws`);
     processSocket.onopen = onOpenProcess;
     processSocket.onmessage = onMessage;
     processSocket.onerror = onerror;
