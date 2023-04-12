94cc33d6629d988a77068006366fd842d2e3daeb
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-04-13 00:13:52 +0800

    fix: 解决证书定时任务错误执行的问题

--- backend/cron/job/ssl.go
+++ backend/cron/job/ssl.go
@@ -3,7 +3,6 @@ package job
 import (
 	"github.com/1Panel-dev/1Panel/backend/app/repo"
 	"github.com/1Panel-dev/1Panel/backend/app/service"
-	"github.com/1Panel-dev/1Panel/backend/constant"
 	"github.com/1Panel-dev/1Panel/backend/global"
 	"time"
 )
@@ -19,19 +18,22 @@ func (ssl *ssl) Run() {
 	sslRepo := repo.NewISSLRepo()
 	sslService := service.NewIWebsiteSSLService()
 	sslList, _ := sslRepo.List()
-	global.LOG.Info("ssl renew cron job start...")
+	global.LOG.Info("The scheduled certificate update task is currently in progress ...")
 	now := time.Now().Add(10 * time.Second)
 	for _, s := range sslList {
 		if !s.AutoRenew || s.Provider == "manual" || s.Provider == "dnsManual" {
 			continue
 		}
-		expireDate, _ := time.ParseInLocation(constant.DateTimeLayout, s.ExpireDate.String(), time.Now().Location())
-		sum := expireDate.Sub(now)
-		if sum.Hours() < 720 {
+		expireDate := s.ExpireDate.In(time.Now().Location())
+		sub := expireDate.Sub(now)
+		if sub.Hours() < 720 {
+			global.LOG.Errorf("Update the SSL certificate for the [%s] domain", s.PrimaryDomain)
 			if err := sslService.Renew(s.ID); err != nil {
-				global.LOG.Errorf("renew doamin [%s] ssl failed err:%s", s.PrimaryDomain, err.Error())
+				global.LOG.Errorf("Failed to update the SSL certificate for the [%s] domain , err:%s", s.PrimaryDomain, err.Error())
+				continue
 			}
+			global.LOG.Errorf("The SSL certificate for the [%s] domain has been successfully updated", s.PrimaryDomain)
 		}
 	}
-	global.LOG.Info("ssl renew cron job end...")
+	global.LOG.Info("The scheduled certificate update task has completed")
 }
