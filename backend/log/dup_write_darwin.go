c76c24e102234b6a885516e7b57c69068cba2042
zhengkunwang223 <31820853+zhengkunwang223@users.noreply.github.com>
2023-05-25 15:06:16 +0800

    feat: 适配 Linux 多架构打包 (#1143)

--- backend/log/dup_write_darwin.go
+++ backend/log/dup_write_darwin.go
@@ -1,16 +1,16 @@
 package log
 
 import (
+	"golang.org/x/sys/unix"
 	"os"
 	"runtime"
-	"syscall"
 )
 
 var stdErrFileHandler *os.File
 
 func dupWrite(file *os.File) error {
 	stdErrFileHandler = file
-	if err := syscall.Dup2(int(file.Fd()), int(os.Stderr.Fd())); err != nil {
+	if err := unix.Dup2(int(file.Fd()), int(os.Stderr.Fd())); err != nil {
 		return err
 	}
 	runtime.SetFinalizer(stdErrFileHandler, func(fd *os.File) {
