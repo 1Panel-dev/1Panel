49f4f316f8691493e624ced90a0353a606a167e9
wanghe-fit2cloud <wanghe@fit2cloud.com>
2023-05-31 15:44:18 +0800

    refactor: goreleaser 增加 dodocker.sh

--- script.sh
+++ script.sh
@@ -17,4 +17,8 @@ if [ ! -f "install.sh" ]; then
   wget https://github.com/1Panel-dev/installer/raw/main/install.sh
 fi
 
+if [ ! -f "docker.sh" ]; then 
+  wget https://github.com/1Panel-dev/installer/raw/main/docker.sh
+fi
+
 chmod 755 1pctl install.sh
