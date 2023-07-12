7dd5082bac6984f1e3b64deb02b025e88c27bb7a
ssongliu <73214554+ssongliu@users.noreply.github.com>
2023-07-12 16:39:16 +0800

    fix: 异常监控数据增加日志打印 (#1622)

--- backend/app/service/monitor.go
+++ backend/app/service/monitor.go
@@ -148,14 +148,16 @@ func loadNetIO() {
 				if net2.BytesSent != 0 && net1.BytesSent != 0 {
 					itemNet.Up = float64(net2.BytesSent-net1.BytesSent) / 1024 / 60
 				}
-				if itemNet.Up > 100000000 {
+				if itemNet.Up > 10485760 {
+					itemNet.Up = 0
 					global.LOG.Errorf("net2: %v, net1: %v, BytesSent: %v \n", net2.BytesSent, net1.BytesSent, float64(net2.BytesSent-net1.BytesSent)/1024/60)
 				}
 
 				if net2.BytesRecv != 0 && net1.BytesRecv != 0 {
 					itemNet.Down = float64(net2.BytesRecv-net1.BytesRecv) / 1024 / 60
 				}
-				if itemNet.Down > 100000000 {
+				if itemNet.Down > 10485760 {
+					itemNet.Down = 0
 					global.LOG.Errorf("net2: %v, net1: %v, BytesRecv: %v \n", net2.BytesRecv, net1.BytesRecv, float64(net2.BytesRecv-net1.BytesRecv)/1024/60)
 				}
 				netList = append(netList, itemNet)
