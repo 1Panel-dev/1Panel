5dac95c3dec3c8a45320fa7b3900e54c1a514d56
ssongliu <73214554+ssongliu@users.noreply.github.com>
2023-08-17 20:24:09 +0800

    fix: 移除容器日志中的 ANSI 转义序列 (#1991)
    Refs #1655

--- frontend/src/views/container/container/log/index.vue
+++ frontend/src/views/container/container/log/index.vue
@@ -147,7 +147,7 @@ const searchLogs = async () => {
         `${protocol}://${host}/api/v1/containers/search/log?container=${logSearch.containerID}&since=${logSearch.mode}&tail=${logSearch.tail}&follow=${logSearch.isWatch}`,
     );
     terminalSocket.value.onmessage = (event) => {
-        logInfo.value += event.data;
+        logInfo.value += event.data.replace(/\x1B\[[0-9;]*[mG]/g, '');
         const state = view.value.state;
         view.value.dispatch({
             selection: { anchor: state.doc.length, head: state.doc.length },
