50589a8d8d034b174a8b31a955261f68e73b0bd8
zhengkunwang <31820853+zhengkunwang223@users.noreply.github.com>
2023-09-18 21:52:20 +0800

    fix: 解决网站设置 PHP 源文之后没有立即生效的问题 (#2341)

--- frontend/src/views/website/website/config/resource/nginx/index.vue
+++ frontend/src/views/website/website/config/resource/nginx/index.vue
@@ -5,7 +5,7 @@
             :placeholder="$t('commons.msg.noneData')"
             :indent-with-tab="true"
             :tabSize="4"
-            style="margin-top: 10px; max-height: 700px"
+            style="max-height: 700px"
             :lineWrapping="true"
             :matchBrackets="true"
             theme="cobalt"
@@ -13,11 +13,9 @@
             :extensions="extensions"
             v-model="content"
         />
-        <div style="margin-top: 10px">
-            <el-button type="primary" @click="submit()">
-                {{ $t('nginx.saveAndReload') }}
-            </el-button>
-        </div>
+        <el-button type="primary" @click="submit()" class="mt-2.5">
+            {{ $t('nginx.saveAndReload') }}
+        </el-button>
     </div>
 </template>
 <script lang="ts" setup>
