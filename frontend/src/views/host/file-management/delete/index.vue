d7f3ab98d1495b9563a220e8ca31f7a53fae627d
ssongliu <songlius11@163.com>
2023-11-08 14:19:18 +0800

    fix: 修改文件删除样式

--- frontend/src/views/host/file-management/delete/index.vue
+++ frontend/src/views/host/file-management/delete/index.vue
@@ -2,16 +2,21 @@
     <el-dialog v-model="open" :title="$t('app.delete')" width="30%" :close-on-click-modal="false">
         <el-row>
             <el-col :span="20" :offset="2">
-                <el-alert :title="$t('file.deleteHelper')" show-icon type="error" :closable="false"></el-alert>
-                <div class="flx-align-center mb-1 mt-1" v-for="(row, index) in files" :key="index">
+                <el-alert class="mt-2" :show-icon="true" type="warning" :closable="false">
+                    <div style="line-height: 20px; word-wrap: break-word">
+                        <span>{{ $t('file.deleteHelper') }}</span>
+                    </div>
+                </el-alert>
+                <div class="mt-4">
+                    <el-checkbox v-model="forceDelete">{{ $t('file.forceDeleteHelper') }}</el-checkbox>
+                </div>
+
+                <div class="flx-align-center mb-1" v-for="(row, index) in files" :key="index">
                     <div>
                         <svg-icon v-if="row.isDir" className="table-icon mr-1 " iconName="p-file-folder"></svg-icon>
                         <svg-icon v-else className="table-icon mr-1" :iconName="getIconName(row.extension)"></svg-icon>
                     </div>
                     <span class="sle">{{ row.name }}</span>
-                </div>
-                <div class="mt-5">
-                    <el-checkbox v-model="forceDelete">{{ $t('file.forceDeleteHelper') }}</el-checkbox>
                 </div>
             </el-col>
         </el-row>
@@ -21,7 +26,7 @@
                 <el-button @click="open = false" :disabled="loading">
                     {{ $t('commons.button.cancel') }}
                 </el-button>
-                <el-button type="primary" @click="onConfirm" v-loading="loading">
+                <el-button type="primary" @click="onConfirm" :disabled="loading">
                     {{ $t('commons.button.confirm') }}
                 </el-button>
             </span>
