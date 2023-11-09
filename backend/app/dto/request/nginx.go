95705bab6e529088b5d52e51e940b73d25f13afa
zhengkunwang223 <zhengkun@fit2cloud.com>
2023-11-09 14:41:31 +0800

    fix: 解决网站保存设置报错的问题

--- backend/app/dto/request/nginx.go
+++ backend/app/dto/request/nginx.go
@@ -14,7 +14,7 @@ type NginxScopeReq struct {
 
 type NginxConfigUpdate struct {
 	Scope     dto.NginxKey `json:"scope"`
-	Operate   string       `json:"operate" validate:"required;oneof=add update delete"`
+	Operate   string       `json:"operate" validate:"required,oneof=add update delete"`
 	WebsiteID uint         `json:"websiteId" validate:"required"`
 	Params    interface{}  `json:"params"`
 }
