fd4c3f2f2e4abf3a5e302d086d6ac66bcdadb4d6
ssongliu <73214554+ssongliu@users.noreply.github.com>
2023-12-04 15:40:09 +0800

    feat: 1pctl 支持修改面板信息 (#3163)

--- cmd/server/cmd/user-info.go
+++ cmd/server/cmd/user-info.go
@@ -3,8 +3,6 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/1Panel-dev/1Panel/backend/global"
-	"github.com/1Panel-dev/1Panel/backend/utils/encrypt"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +12,7 @@ func init() {
 
 var userinfoCmd = &cobra.Command{
 	Use:   "user-info",
-	Short: "获取用户信息",
+	Short: "获取面板信息",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if !isRoot() {
 			fmt.Println("请使用 sudo 1pctl user-info 或者切换到 root 用户")
@@ -25,20 +23,10 @@ var userinfoCmd = &cobra.Command{
 			return fmt.Errorf("init my db conn failed, err: %v \n", err)
 		}
 		user := getSettingByKey(db, "UserName")
-		password := getSettingByKey(db, "Password")
 		port := getSettingByKey(db, "ServerPort")
 		ssl := getSettingByKey(db, "SSL")
 		entrance := getSettingByKey(db, "SecurityEntrance")
-		encryptSetting := getSettingByKey(db, "EncryptKey")
 		address := getSettingByKey(db, "SystemIP")
-
-		p := ""
-		if len(encryptSetting) == 16 {
-			global.CONF.System.EncryptKey = encryptSetting
-			p, _ = encrypt.StringDecrypt(password)
-		} else {
-			p = password
-		}
 
 		protocol := "http"
 		if ssl == "enable" {
@@ -49,8 +37,9 @@ var userinfoCmd = &cobra.Command{
 		}
 
 		fmt.Printf("面板地址: %s://%s:%s/%s \n", protocol, address, port, entrance)
-		fmt.Printf("用户名称: %s\n", user)
-		fmt.Printf("用户密码: %s\n", p)
+		fmt.Println("面板用户: ", user)
+		fmt.Println("面板密码: ", "********")
+		fmt.Println("提示：修改密码可执行命令：1pctl update password")
 		return nil
 	},
 }
