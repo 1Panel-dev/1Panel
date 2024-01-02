58cde0b07908d8426edba254992505342194f88f
ssongliu <songlius11@163.com>
2024-01-02 17:03:33 +0800

    fix: 解决工具箱 Hosts 解析失败的问题

--- frontend/src/views/toolbox/device/hosts/index.vue
+++ frontend/src/views/toolbox/device/hosts/index.vue
@@ -7,6 +7,12 @@
 
             <el-row type="flex" justify="center" v-loading="loading">
                 <el-col :span="22">
+                    <el-alert
+                        :title="$t('toolbox.device.timeZoneHelper')"
+                        class="common-prompt"
+                        :closable="false"
+                        type="warning"
+                    />
                     <el-radio-group v-model="confShowType" @change="changeMode">
                         <el-radio-button label="base">{{ $t('database.baseConf') }}</el-radio-button>
                         <el-radio-button label="all">{{ $t('database.allConf') }}</el-radio-button>
