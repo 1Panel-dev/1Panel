28e8bf17fa8fe8e23d1b5c111d43fad4c61dd901
ssongliu <songlius11@163.com>
2024-03-15 14:14:49 +0800

    fix: 解决未登录情况下的页面跳转问题

--- frontend/src/routers/index.ts
+++ frontend/src/routers/index.ts
@@ -5,15 +5,12 @@ import { AxiosCanceler } from '@/api/helper/axios-cancel';
 
 const axiosCanceler = new AxiosCanceler();
 
-/**
- * @description 路由拦截 beforeEach（路由配置无数种方法，个人觉得最简便）
- * */
 router.beforeEach((to, from, next) => {
     NProgress.start();
     axiosCanceler.removeAllPending();
     const globalStore = GlobalStore();
 
-    if (to.name === 'home' && !globalStore.isLogin) {
+    if (to.name !== 'entrance' && !globalStore.isLogin) {
         next({
             name: 'entrance',
             params: { code: globalStore.entrance },
