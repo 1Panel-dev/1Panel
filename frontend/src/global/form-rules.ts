f3267bf13737744be6ffc93371321e7cd71ff4b8
zhengkunwang <31820853+zhengkunwang223@users.noreply.github.com>
2024-05-09 11:32:23 +0800

    style: 去掉多余的字符 (#4936)

--- frontend/src/global/form-rules.ts
+++ frontend/src/global/form-rules.ts
@@ -237,7 +237,7 @@ const checkSupervisorName = (rule: any, value: any, callback: any) => {
     if (value === '' || typeof value === 'undefined' || value == null) {
         callback(new Error(i18n.global.t('commons.rule.supervisorName')));
     } else {
-        const reg = /^[a-zA-Z0-9]{1}[a-zA-Z0-9_-]{0,128}$/;
+        const reg = /^[a-zA-Z0-9]{1}[a-zA-Z0-9_-]{0,127}$/;
         if (!reg.test(value) && value !== '') {
             callback(new Error(i18n.global.t('commons.rule.supervisorName')));
         } else {
