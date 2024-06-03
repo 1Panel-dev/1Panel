ceca7fade000a5a1b6c969f7fcef93f8804e379a
zhengkunwang <31820853+zhengkunwang223@users.noreply.github.com>
2024-06-03 17:55:46 +0800

    fix: 解决部分情况下升级版本不显示的问题 (#5261)
    Refs https://github.com/1Panel-dev/1Panel/issues/5256

--- backend/app/model/app.go
+++ backend/app/model/app.go
@@ -16,7 +16,7 @@ type App struct {
 	Type               string `json:"type" gorm:"type:varchar(64);not null"`
 	Status             string `json:"status" gorm:"type:varchar(64);not null"`
 	Required           string `json:"required" gorm:"type:varchar(64);"`
-	CrossVersionUpdate bool   `json:"crossVersionUpdate"`
+	CrossVersionUpdate bool   `json:"crossVersionUpdate" yaml:"crossVersionUpdate"`
 	Limit              int    `json:"limit" gorm:"type:Integer;not null"`
 	Website            string `json:"website" gorm:"type:varchar(64);not null"`
 	Github             string `json:"github" gorm:"type:varchar(64);not null"`
