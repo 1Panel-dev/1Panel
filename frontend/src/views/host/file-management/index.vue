a885fe2f70c64b27d98e37b457065fae9a2051f4
lan-yonghui <yonghui.lan@fit2cloud.com>
2024-07-16 14:14:43 +0800

    fix: 文本管理计算文件夹大小调整为局部加载,增加请求时长

--- frontend/src/views/host/file-management/index.vue
+++ frontend/src/views/host/file-management/index.vue
@@ -260,7 +260,13 @@
                     <el-table-column :label="$t('file.size')" prop="size" max-width="50" sortable>
                         <template #default="{ row, $index }">
                             <span v-if="row.isDir">
-                                <el-button type="primary" link small @click="getDirSize(row, $index)">
+                                <el-button
+                                    type="primary"
+                                    link
+                                    small
+                                    @click="getDirSize(row, $index)"
+                                    :loading="btnLoading == $index"
+                                >
                                     <span v-if="row.dirSize == undefined">
                                         {{ $t('file.calculate') }}
                                     </span>
@@ -381,6 +387,7 @@ const initData = () => ({
 });
 let req = reactive(initData());
 let loading = ref(false);
+let btnLoading = ref(-1);
 const paths = ref<FilePaths[]>([]);
 let pathWidth = ref(0);
 const history: string[] = [];
@@ -631,7 +638,7 @@ const getDirSize = async (row: any, index: number) => {
     const req = {
         path: row.path,
     };
-    loading.value = true;
+    btnLoading.value = index;
     await ComputeDirSize(req)
         .then(async (res) => {
             let newData = [...data.value];
@@ -639,7 +646,7 @@ const getDirSize = async (row: any, index: number) => {
             data.value = newData;
         })
         .finally(() => {
-            loading.value = false;
+            btnLoading.value = -1;
         });
 };
 
