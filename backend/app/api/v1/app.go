47ab064d46adc40a77026c08130dcb1bd3c74f96
ssonglius11 <ssonglius11@163.com>
2024-07-23 15:41:39 +0800

    fix: 解决系统日志解析错误的问题

--- backend/app/api/v1/app.go
+++ backend/app/api/v1/app.go
@@ -155,7 +155,7 @@ func (b *BaseApi) GetIgnoredApp(c *gin.Context) {
 // @Success 200 {object} model.AppInstall
 // @Security ApiKeyAuth
 // @Router /apps/install [post]
-// @x-panel-log {"bodyKeys":["name"],"paramKeys":[],"BeforeFunctions":[{"input_column":"name","input_value":"name","isList":false,"db":"app_installs","output_column":"app_id","output_value":"appId"},{"info":"appId","isList":false,"db":"apps","output_column":"key","output_value":"appKey"}],"formatZH":"安装应用 [appKey]-[name]","formatEN":"Install app [appKey]-[name]"}
+// @x-panel-log {"bodyKeys":["name"],"paramKeys":[],"BeforeFunctions":[],"formatZH":"安装应用 [name]","formatEN":"Install app [name]"}
 func (b *BaseApi) InstallApp(c *gin.Context) {
 	var req request.AppInstallCreate
 	if err := helper.CheckBindAndValidate(&req, c); err != nil {
--- backend/middleware/operation.go
+++ backend/middleware/operation.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"reflect"
 	"strings"
 	"time"
 
@@ -110,12 +111,21 @@ func OperationLog() gin.HandlerFunc {
 		}
 		for key, value := range formatMap {
 			if strings.Contains(operationDic.FormatEN, "["+key+"]") {
-				if arrays, ok := value.([]string); ok {
-					operationDic.FormatZH = strings.ReplaceAll(operationDic.FormatZH, "["+key+"]", fmt.Sprintf("[%v]", strings.Join(arrays, ",")))
-					operationDic.FormatEN = strings.ReplaceAll(operationDic.FormatEN, "["+key+"]", fmt.Sprintf("[%v]", strings.Join(arrays, ",")))
-				} else {
+				t := reflect.TypeOf(value)
+				if t.Kind() != reflect.Array && t.Kind() != reflect.Slice {
 					operationDic.FormatZH = strings.ReplaceAll(operationDic.FormatZH, "["+key+"]", fmt.Sprintf("[%v]", value))
 					operationDic.FormatEN = strings.ReplaceAll(operationDic.FormatEN, "["+key+"]", fmt.Sprintf("[%v]", value))
+				} else {
+					val := reflect.ValueOf(value)
+					length := val.Len()
+
+					var elements []string
+					for i := 0; i < length; i++ {
+						element := val.Index(i).Interface().(string)
+						elements = append(elements, element)
+					}
+					operationDic.FormatZH = strings.ReplaceAll(operationDic.FormatZH, "["+key+"]", fmt.Sprintf("[%v]", strings.Join(elements, ",")))
+					operationDic.FormatEN = strings.ReplaceAll(operationDic.FormatEN, "["+key+"]", fmt.Sprintf("[%v]", strings.Join(elements, ",")))
 				}
 			}
 		}
