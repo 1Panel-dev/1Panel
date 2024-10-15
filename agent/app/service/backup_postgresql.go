f3fd02fcb3dd6d78a0df4ba60e5652792fcf0f35
ssongliu <73214554+ssongliu@users.noreply.github.com>
2024-10-15 16:03:21 +0800

    feat: 计划任务备份数据库增加任务日志 (#6717)

--- agent/app/service/backup_postgresql.go
+++ agent/app/service/backup_postgresql.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/1Panel-dev/1Panel/agent/constant"
+	"github.com/1Panel-dev/1Panel/agent/i18n"
 
 	"github.com/1Panel-dev/1Panel/agent/buserr"
 	"github.com/1Panel-dev/1Panel/agent/utils/common"
@@ -16,6 +17,7 @@ import (
 
 	"github.com/1Panel-dev/1Panel/agent/app/dto"
 	"github.com/1Panel-dev/1Panel/agent/app/model"
+	"github.com/1Panel-dev/1Panel/agent/app/task"
 	"github.com/1Panel-dev/1Panel/agent/global"
 	"github.com/1Panel-dev/1Panel/agent/utils/files"
 	"github.com/1Panel-dev/1Panel/agent/utils/postgresql/client"
@@ -27,7 +29,8 @@ func (u *BackupService) PostgresqlBackup(req dto.CommonBackup) error {
 	targetDir := path.Join(global.CONF.System.Backup, itemDir)
 	fileName := fmt.Sprintf("%s_%s.sql.gz", req.DetailName, timeNow+common.RandStrAndNum(5))
 
-	if err := handlePostgresqlBackup(req.Name, req.DetailName, targetDir, fileName); err != nil {
+	databaseHelper := DatabaseHelper{Database: req.Name, DBType: req.Type, Name: req.DetailName}
+	if err := handlePostgresqlBackup(databaseHelper, nil, targetDir, fileName, req.TaskID); err != nil {
 		return err
 	}
 
@@ -46,7 +49,7 @@ func (u *BackupService) PostgresqlBackup(req dto.CommonBackup) error {
 	return nil
 }
 func (u *BackupService) PostgresqlRecover(req dto.CommonRecover) error {
-	if err := handlePostgresqlRecover(req, false); err != nil {
+	if err := handlePostgresqlRecover(req, nil, false); err != nil {
 		return err
 	}
 	return nil
@@ -90,85 +93,123 @@ func (u *BackupService) PostgresqlRecoverByUpload(req dto.CommonRecover) error {
 	}
 
 	req.File = path.Dir(file) + "/" + fileName
-	if err := handlePostgresqlRecover(req, false); err != nil {
+	if err := handlePostgresqlRecover(req, nil, false); err != nil {
 		return err
 	}
 	global.LOG.Info("recover from uploads successful!")
 	return nil
 }
-func handlePostgresqlBackup(database, dbName, targetDir, fileName string) error {
-	cli, err := LoadPostgresqlClientByFrom(database)
-	if err != nil {
-		return err
-	}
-	defer cli.Close()
 
-	backupInfo := pgclient.BackupInfo{
-		Name:      dbName,
-		TargetDir: targetDir,
-		FileName:  fileName,
+func handlePostgresqlBackup(db DatabaseHelper, parentTask *task.Task, targetDir, fileName, taskID string) error {
+	var (
+		err      error
+		itemTask *task.Task
+	)
+	itemTask = parentTask
+	itemName := fmt.Sprintf("%s - %s", db.Database, db.Name)
+	if parentTask == nil {
+		itemTask, err = task.NewTaskWithOps(itemName, task.TaskBackup, task.TaskScopeDatabase, taskID, db.ID)
+		if err != nil {
+			return err
+		}
+	}
+	backupDatabase := func(t *task.Task) error {
+		cli, err := LoadPostgresqlClientByFrom(db.Database)
+		if err != nil {
+			return err
+		}
+		defer cli.Close()
+		backupInfo := pgclient.BackupInfo{
+			Name:      db.Name,
+			TargetDir: targetDir,
+			FileName:  fileName,
 
-		Timeout: 300,
+			Timeout: 300,
+		}
+		return cli.Backup(backupInfo)
 	}
-	if err := cli.Backup(backupInfo); err != nil {
-		return err
+	itemTask.AddSubTask(i18n.GetMsgByKey("TaskBackup"), backupDatabase, nil)
+	if parentTask != nil {
+		return backupDatabase(parentTask)
 	}
-	return nil
+
+	return itemTask.Execute()
 }
 
-func handlePostgresqlRecover(req dto.CommonRecover, isRollback bool) error {
-	isOk := false
-	fileOp := files.NewFileOp()
-	if !fileOp.Stat(req.File) {
-		return buserr.WithName("ErrFileNotFound", req.File)
-	}
+func handlePostgresqlRecover(req dto.CommonRecover, parentTask *task.Task, isRollback bool) error {
+	var (
+		err      error
+		itemTask *task.Task
+	)
 	dbInfo, err := postgresqlRepo.Get(commonRepo.WithByName(req.DetailName), postgresqlRepo.WithByPostgresqlName(req.Name))
 	if err != nil {
 		return err
 	}
-	cli, err := LoadPostgresqlClientByFrom(req.Name)
-	if err != nil {
-		return err
-	}
-	defer cli.Close()
-
-	if !isRollback {
-		rollbackFile := path.Join(global.CONF.System.TmpDir, fmt.Sprintf("database/%s/%s_%s.sql.gz", req.Type, req.DetailName, time.Now().Format(constant.DateTimeSlimLayout)))
-		if err := cli.Backup(client.BackupInfo{
-			Name:      req.DetailName,
-			TargetDir: path.Dir(rollbackFile),
-			FileName:  path.Base(rollbackFile),
-
-			Timeout: 300,
-		}); err != nil {
-			return fmt.Errorf("backup postgresql db %s for rollback before recover failed, err: %v", req.DetailName, err)
+	itemTask = parentTask
+	if parentTask == nil {
+		itemTask, err = task.NewTaskWithOps("Redis", task.TaskRecover, task.TaskScopeDatabase, req.TaskID, dbInfo.ID)
+		if err != nil {
+			return err
 		}
-		defer func() {
-			if !isOk {
-				global.LOG.Info("recover failed, start to rollback now")
-				if err := cli.Recover(client.RecoverInfo{
-					Name:       req.DetailName,
-					SourceFile: rollbackFile,
+	}
 
-					Timeout: 300,
-				}); err != nil {
-					global.LOG.Errorf("rollback postgresql db %s from %s failed, err: %v", req.DetailName, rollbackFile, err)
-				}
-				global.LOG.Infof("rollback postgresql db %s from %s successful", req.DetailName, rollbackFile)
-				_ = os.RemoveAll(rollbackFile)
-			} else {
-				_ = os.RemoveAll(rollbackFile)
+	recoverDatabase := func(t *task.Task) error {
+		isOk := false
+		fileOp := files.NewFileOp()
+		if !fileOp.Stat(req.File) {
+			return buserr.WithName("ErrFileNotFound", req.File)
+		}
+
+		cli, err := LoadPostgresqlClientByFrom(req.Name)
+		if err != nil {
+			return err
+		}
+		defer cli.Close()
+
+		if !isRollback {
+			rollbackFile := path.Join(global.CONF.System.TmpDir, fmt.Sprintf("database/%s/%s_%s.sql.gz", req.Type, req.DetailName, time.Now().Format(constant.DateTimeSlimLayout)))
+			if err := cli.Backup(client.BackupInfo{
+				Name:      req.DetailName,
+				TargetDir: path.Dir(rollbackFile),
+				FileName:  path.Base(rollbackFile),
+
+				Timeout: 300,
+			}); err != nil {
+				return fmt.Errorf("backup postgresql db %s for rollback before recover failed, err: %v", req.DetailName, err)
 			}
-		}()
+			defer func() {
+				if !isOk {
+					global.LOG.Info("recover failed, start to rollback now")
+					if err := cli.Recover(client.RecoverInfo{
+						Name:       req.DetailName,
+						SourceFile: rollbackFile,
+
+						Timeout: 300,
+					}); err != nil {
+						global.LOG.Errorf("rollback postgresql db %s from %s failed, err: %v", req.DetailName, rollbackFile, err)
+					}
+					global.LOG.Infof("rollback postgresql db %s from %s successful", req.DetailName, rollbackFile)
+					_ = os.RemoveAll(rollbackFile)
+				} else {
+					_ = os.RemoveAll(rollbackFile)
+				}
+			}()
+		}
+		if err := cli.Recover(client.RecoverInfo{
+			Name:       req.DetailName,
+			SourceFile: req.File,
+			Username:   dbInfo.Username,
+			Timeout:    300,
+		}); err != nil {
+			return err
+		}
+		isOk = true
+		return nil
 	}
-	if err := cli.Recover(client.RecoverInfo{
-		Name:       req.DetailName,
-		SourceFile: req.File,
-		Username:   dbInfo.Username,
-		Timeout:    300,
-	}); err != nil {
-		return err
+	itemTask.AddSubTask(i18n.GetMsgByKey("TaskRecover"), recoverDatabase, nil)
+	if parentTask != nil {
+		return recoverDatabase(parentTask)
 	}
-	isOk = true
-	return nil
+
+	return itemTask.Execute()
 }
