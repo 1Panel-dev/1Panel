61d285a5415e43dae8526ea44a2789cb37f9a2c7
lan-yonghui <yonghui.lan@fit2cloud.com>
2025-05-12 17:08:49 +0800

    feat: Adjust offline alert settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -54,11 +54,7 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
             host: '0.0.0.0',
             proxy: {
                 '/api/v2': {
-                    // target: 'http://192.168.3.47:9999/',
-                    target: 'http://172.16.10.109:9999/',
-                    // target: 'http://172.16.10.181:9999/',
-                    // target: 'http://47.83.11.120:9999/',
-                    // target: 'http://47.76.165.8:9999/',
+                    target: 'http://localhost:9999/',
                     changeOrigin: true,
                     ws: true,
                 },
