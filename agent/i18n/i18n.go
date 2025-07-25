d84e4452b8ef3205ec053a4f0715e97952d5d479
CEO DaXyS <20468345+R3N3G@users.noreply.github.com>
2025-07-25 15:58:37 +0200

    Update i18n.go

--- agent/i18n/i18n.go
+++ agent/i18n/i18n.go
@@ -134,6 +134,7 @@ func Init() {
 	_, _ = bundle.LoadMessageFileFS(fs, "lang/ms.yaml")
 	_, _ = bundle.LoadMessageFileFS(fs, "lang/ko.yaml")
 	_, _ = bundle.LoadMessageFileFS(fs, "lang/tr.yaml")
+	_, _ = bundle.LoadMessageFileFS(fs, "lang/es.yaml")
 	lang := GetLanguageFromDB()
 	global.I18n = i18n.NewLocalizer(bundle, lang)
 }
