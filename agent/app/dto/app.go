dee66a4b1e19358248b14c5fa18baf726ecf474a
CEO DaXyS <20468345+R3N3G@users.noreply.github.com>
2025-07-25 16:06:04 +0200

    Update app.go

--- agent/app/dto/app.go
+++ agent/app/dto/app.go
@@ -128,7 +128,7 @@ type Locale struct {
 	Zh     string `json:"zh"`
 	Ko     string `json:"ko"`
 	Tr     string `json:"tr"`
-	Es     string `json:"es"`
+	Es     string `json:"es-es" yaml:"es-es"`
 }
 
 type AppForm struct {
