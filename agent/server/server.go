a8592da72edf0c2df247903921e9dab2667f856d
HynoR <20227709+HynoR@users.noreply.github.com>
2025-10-31 20:52:11 +0800

    fix: change-mode

--- agent/server/server.go
+++ agent/server/server.go
@@ -4,10 +4,11 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/1Panel-dev/1Panel/agent/app/repo"
 	"github.com/1Panel-dev/1Panel/agent/constant"
@@ -40,6 +41,9 @@ func Start() {
 	app.Init()
 	lang.Init()
 	validator.Init()
+	if os.Getenv("GIN_MODE") == "" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	cron.Run()
 	hook.Init()
 	InitOthers()
