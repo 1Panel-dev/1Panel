4f4bfd372bbc61c70ead0135158ea8837aaa1a56
HynoR <20227709+HynoR@users.noreply.github.com>
2025-12-08 13:19:38 +0800

    revert: enable clean log button

--- frontend/src/components/log/container/index.vue
+++ frontend/src/components/log/container/index.vue
@@ -20,7 +20,7 @@
         <el-button class="margin-button" @click="onDownload" icon="Download">
             {{ $t('commons.button.download') }}
         </el-button>
-        <el-button class="margin-button" @click="onClean" icon="Delete" v-if="logSearch.container !== ''">
+        <el-button class="margin-button" @click="onClean" icon="Delete">
             {{ $t('commons.button.clean') }}
         </el-button>
     </div>
