1d0d954c8d0ade6da11983832f8ba5186d706d1f
lan-yonghui <yonghui.lan@fit2cloud.com>
2026-06-15 15:10:02 +0800

    feat: add QR code verification endpoint to password expiration checks

--- core/middleware/password_expired.go
+++ core/middleware/password_expired.go
@@ -27,6 +27,7 @@ func PasswordExpired() gin.HandlerFunc {
 			c.Request.URL.Path == "/api/v2/core/settings/search" ||
 			c.Request.URL.Path == "/api/v2/core/settings/search/base" ||
 			c.Request.URL.Path == "/api/v2/core/xpack/settings/search" ||
+			c.Request.URL.Path == "/api/v2/core/xapp/verifyQRCode" ||
 			c.Request.URL.Path == "/api/v2/core/enterprise/users/info" ||
 			c.Request.URL.Path == "/api/v2/core/enterprise/licenses/info" ||
 			c.Request.URL.Path == "/api/v2/core/enterprise/licenses/status" ||
